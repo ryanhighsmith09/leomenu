2e01e90b4c814d02f8e8f330cc619c7b947d0ce3
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-01-31 14:52:20 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,31 @@
 
+Config = {}
+
+Config.MenuKey = 'M'
+Config.Align = 'left' -- left / right
+Config.UseLOS = true
+Config.MaxDistance = 3.0
+
+Config.Cooldowns = {
+    cuff = 3,
+    escort = 2,
+    jailJob = 60
+}
+
+Config.Duty = {
+    useCommand = true,
+    command = 'duty',
+    locations = {
+        vec3(441.2, -981.9, 30.6)
+    }
+}
+
+Config.Departments = {
+    LSPD = { label = 'LSPD', loadout = { 'weapon_pistol' } },
+    BCSO = { label = 'BCSO', loadout = { 'weapon_smg' } }
+}
+
+Config.Jail = {
+    entry = vec3(459.9, -994.3, 24.9),
+    exit = vec3(1846.1, 2585.9, 45.7)
+}
