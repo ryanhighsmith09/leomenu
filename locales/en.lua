95f90022e0129bfda2dceab70abb77e396cda805
ryanhighsmith09 <ryanhighsmith09@gmail.com>
2026-01-31 14:54:04 -0600

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales = {}
+
+Locales.en = {
+    cuff = 'Cuff / Uncuff',
+    escort = 'Escort / Stop Escort',
+    putveh = 'Put in Vehicle',
+    outveh = 'Remove from Vehicle',
+    duty_on = 'You are now on duty',
+    duty_off = 'You are now off duty',
+    panic = 'PANIC BUTTON ACTIVATED'
+}
